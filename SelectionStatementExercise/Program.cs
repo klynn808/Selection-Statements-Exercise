a020f3a424f8ddc9f4876c42ee05769c10d0d32d
Kate Lynn Thomson <katelynn.jaremko@gmail.com>
2024-01-12 09:51:50 -0600

    exercise commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var r = new Random();
+            //var faveNumber = r.Next(1, 10);
+            var faveNumber = 7;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < faveNumber)
+            {
+                Console.WriteLine($"{userInput} is too low, try again!");
+            }
+            else if (userInput > faveNumber) 
+            {
+                 Console.WriteLine($"{userInput} is too high, try it again!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it just right, congrats!");
+            }
         }
     }
 }
